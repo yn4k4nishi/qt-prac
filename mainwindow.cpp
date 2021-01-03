4ff234dab57108de4011f62ae0c933d8972443b5
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-01-03 17:52:18 +0900

    fix : cmakelists.txt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
+    
     connect(ui->OK, &QPushButton::released, this, &MainWindow::pushOKButton);
     connect(ui->Cansel, &QPushButton::released, this, &MainWindow::pushCanselButton);
 
