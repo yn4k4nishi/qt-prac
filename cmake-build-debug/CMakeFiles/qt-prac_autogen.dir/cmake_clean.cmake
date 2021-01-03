7266338a7646ac6db8eaeb5a6c7c5b4034b743e0
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-01-03 17:33:13 +0900

    remove build files

--- cmake-build-debug/CMakeFiles/qt-prac_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/qt-prac_autogen"
-  "qt-prac_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/qt-prac_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
